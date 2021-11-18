7f925ec682f97513ae442a944351c119aecec490
Ginger Heef <gingerheef003@gmail.com>
2021-11-18 17:45:02 +0530

    sum

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a = 1;
+    float b = 2.3;
+    double c = 4.5;
+
+    float sum = a + b + c;
+
+    printf("The sum of a, b, c is %f", sum);
+
+    return 0;
+}
